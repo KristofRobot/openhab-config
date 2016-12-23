d4b7af48e4b4ce67cab7aa72aba1cb42c3f02893
Kristof Robot <krirobo@gmail.com>
2016-12-23 08:06:00 +0000

    Tuning update sections

--- configurations/rules_refresh.sh
+++ configurations/rules_refresh.sh
@@ -1,5 +1,5 @@
 # This script is called by openHAB after persistance service was started
-sleep 10 
+sleep 30 
 cd /etc/openhab/configurations/rules/
 RULES=`find *.rules | grep -v refresh.rules`
 for f in $RULES
