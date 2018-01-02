0ab2aacfb9a2b46896c80a93e1653355d1f5ee20
Kristof Robot <krirobo@gmail.com>
2018-01-02 18:42:43 +0100

    CO2Monitor: initial commit

---
+++ sensors/CO2Monitor/README.md
@@ -0,0 +1,37 @@
+# ESP8266-co2monitor
+
+## description
+
+Interface an TFA Dostmann CO2 Monitor with ESP8266 (in our example a WEMOS D1 Mini) to connect it to the "Internet of Things". It publishes the CO2 measurement and temperature to a configures MQTT topic.
+
+## compiling
+
+* Rename settings.h.example to settings.h
+* Open up your arduino IDE
+* Configure your OTA, Wifi, MQTT Topics in the settings.h file
+* Upload the compiled result to your Wemos D1 Mini
+
+## wiring
+
+Add a pin header to the existing PCB. Left to right: GND, Clock, Data, 5V:
+<br>
+<a href="https://github.com/b4ckspace/esp8266-co2monitor/blob/master/doc/images/pinheader.jpg?raw=true">
+    <img alt="PIN Header" src="https://github.com/b4ckspace/esp8266-co2monitor/blob/master/doc/images/pinheader-thumb.jpg?raw=true">
+</a>
+
+Wire up your Wemos with 4 wires, connected to 5V, G, D4 and D3:
+<br>
+<a href="https://github.com/b4ckspace/esp8266-co2monitor/blob/master/doc/images/wemos-wiring.jpg?raw=true">
+    <img alt="Wemos Wiring" src="https://github.com/b4ckspace/esp8266-co2monitor/blob/master/doc/images/wemos-wiring-thumb.jpg?raw=true">
+</a>
+
+Now connect your Wemos D1 mini to your co2 monitor:
+<br>
+<a href="https://github.com/b4ckspace/esp8266-co2monitor/blob/master/doc/images/wiring.jpg?raw=true">
+    <img alt="Final Wiring" src="https://github.com/b4ckspace/esp8266-co2monitor/blob/master/doc/images/wiring-thumb.jpg?raw=true">
+</a>
+
+## dependencies
+
+* PubSubClient
+* Arduino/ESP8266
---
+++ sensors/CO2Monitor/esp8266-co2monitor.ino
@@ -0,0 +1,167 @@
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include "settings.h"
+
+#define IDX_CMD 0
+#define IDX_MSB 1
+#define IDX_LSB 2
+#define IDX_CHECKSUM 3
+#define IDX_END 4
+
+#define CMD_TEMPERATURE 0x42
+#define CMD_CO2_MEASUREMENT 0x50
+
+WiFiClient wifiClient;
+PubSubClient mqttClient;
+
+uint8_t bitIndex = 0;
+uint8_t byteIndex = 0;
+uint8_t clkValue = LOW;
+uint8_t lastClkValue = LOW;
+
+uint8_t tmp = 0;
+unsigned long currentMillis = 0;
+unsigned long lastMillis = 0;
+unsigned long lastUpdateMs = 0;
+
+uint16_t co2Measurement = 0;
+float smoothCo2Measurement = 0.0;
+
+float temperature = 0;
+
+byte bits[8];
+byte bytes[5] = {0};
+
+char sprintfHelper[16] = {0};
+
+//static IP
+IPAddress WIFI_IP(1,1,1,1); 
+IPAddress WIFI_GW(1,1,1,1); 
+IPAddress WIFI_SUBNET(255, 255, 255, 0); 
+
+void setup() {
+
+  // Power up wait
+  delay(2000);
+  
+  Serial.begin(115200);
+  Serial.println("Hello");
+
+  pinMode(PIN_CLK, INPUT);
+  pinMode(PIN_DATA, INPUT);
+
+  WiFi.config(WIFI_IP, WIFI_GW, WIFI_SUBNET);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  mqttClient.setClient(wifiClient);
+  mqttClient.setServer(IPAddress(10, 10, 0, 4), 1883);
+
+  mqttConnect();
+
+  //move to end, otherwise interferes with setting up of WiFi
+  attachInterrupt(PIN_CLK, onClock, RISING);
+}
+
+void onClock() {
+
+  lastMillis = millis();
+  bits[bitIndex++] = (digitalRead(PIN_DATA) == HIGH) ? 1 : 0;
+
+  // Transform bits to byte
+  if (bitIndex >= 8) {
+    tmp = 0;
+    for (uint8_t i = 0; i < 8; i++) {
+      tmp |= (bits[i] << (7 - i));
+    }
+
+    bytes[byteIndex++] = tmp;
+    bitIndex = 0;
+  }
+
+  if (byteIndex >= 5) {
+    byteIndex = 0;
+    decodeDataPackage(bytes);
+  }
+}
+
+void mqttConnect() {
+  while (!mqttClient.connected()) {
+    if (mqttClient.connect(WIFI_HOSTNAME, MQTT_TOPIC_SENSOR_STATE, 1, true, "disconnected")) {
+      mqttClient.publish(MQTT_TOPIC_SENSOR_STATE, "connected", true);
+    } else {
+    }
+  }
+}
+
+void loop() {
+  currentMillis = millis();
+
+  // Over 50ms no bits? Reset!
+  if (currentMillis - lastMillis > 50) {
+    bitIndex = 0;
+    byteIndex = 0;
+  }
+
+  long updateInterval = PUBLISH_INTERVAL_SLOW_MS;
+
+  // If the change is above a specific threshold, we update faster!
+  float percentChange = abs(((float) co2Measurement / smoothCo2Measurement) - 1.0);   
+  if (percentChange > 0.05) {
+    updateInterval = PUBLISH_INTERVAL_FAST_MS;
+  }
+
+  if (currentMillis - lastUpdateMs > updateInterval) {
+    lastUpdateMs = millis();
+
+    if (co2Measurement > 0) {
+      sprintf(sprintfHelper, "%d", co2Measurement);
+      mqttClient.publish(MQTT_TOPIC_CO2_MEASUREMENT, sprintfHelper, true);
+    }
+
+    if (temperature > 0) {
+      dtostrf(temperature, 4, 2, sprintfHelper);
+      mqttClient.publish(MQTT_TOPIC_TEMPERATURE_MEASUREMENT, sprintfHelper, true);
+    }
+
+  }
+
+  mqttConnect();
+  mqttClient.loop();
+}
+
+bool decodeDataPackage(byte data[5]) {
+
+  if (data[IDX_END] != 0x0D) {
+    return false;
+  }
+
+  uint8_t checksum = data[IDX_CMD] + data[IDX_MSB] + data[IDX_LSB];
+  if (data[IDX_CHECKSUM] != checksum) {
+    return false;
+  }
+
+  switch (data[IDX_CMD]) {
+    case CMD_CO2_MEASUREMENT:
+      co2Measurement = (data[IDX_MSB] << 8) | data[IDX_LSB];
+
+      // Exponential smoothing
+      smoothCo2Measurement = EXP_SMOOTH_ALPHA * (float) co2Measurement + (1.0 - EXP_SMOOTH_ALPHA) * smoothCo2Measurement;
+
+      Serial.print("CO2: ");
+      Serial.println(co2Measurement);
+      break;
+    case CMD_TEMPERATURE:
+      temperature = ((data[IDX_MSB] << 8) | data[IDX_LSB]) / 16.0 - 273.15;
+      Serial.print("Temp: ");
+      Serial.println(temperature);
+      break;
+  }
+
+}
+
